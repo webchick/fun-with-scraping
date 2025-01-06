e48e4c8ef67cec6095cf72aeb3e26776fb73d519
Angie Byron <webchick@webchick.net>
2025-01-05 23:47:54 -0800

    First, we add the where

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-// Let's scrape something...
+import { RequestQueue } from 'crawlee';
+
+// First you create the request queue instance.
+const requestQueue = await RequestQueue.open();
+
+// And then you add one or more requests to it.
+await requestQueue.addRequest({ url: 'https://crawlee.dev' });
