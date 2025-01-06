21ff78f6496cc3a8a3918d5c7e8d39c330588de0
Angie Byron <webchick@webchick.net>
2025-01-05 23:48:40 -0800

    Next, we add the CheerioCrawler

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import { RequestQueue } from 'crawlee';
+// Add import of CheerioCrawler
+import { RequestQueue, CheerioCrawler } from 'crawlee';
 
-// First you create the request queue instance.
 const requestQueue = await RequestQueue.open();
-
-// And then you add one or more requests to it.
 await requestQueue.addRequest({ url: 'https://crawlee.dev' });
+
+// Create the crawler and add the queue with our URL
+// and a request handler to process the page.
+const crawler = new CheerioCrawler({
+    requestQueue,
+    // The `$` argument is the Cheerio object
+    // which contains parsed HTML of the website.
+    async requestHandler({ $, request }) {
+        // Extract <title> text with Cheerio.
+        // See Cheerio documentation for API docs.
+        const title = $('title').text();
+        console.log(`The title of "${request.url}" is: ${title}.`);
+    }
+})
+
+// Start the crawler and wait for it to finish
+await crawler.run();
