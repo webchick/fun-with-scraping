20048968c1be783192925c6fd6761f9f18122fc8
Angie Byron <webchick@webchick.net>
2025-01-06 00:59:00 -0800

    Crawl the detail pages

--- src/main.js
+++ src/main.js
@@ -3,16 +3,36 @@ import { PlaywrightCrawler } from 'crawlee';
 const crawler = new PlaywrightCrawler({
     requestHandler: async ({ page, request, enqueueLinks }) => {
         console.log(`Processing: ${request.url}`);
-        // Wait for the category cards to render,
-        // otherwise enqueueLinks wouldn't enqueue anything.
-        await page.waitForSelector('.collection-block-item');
+        if (request.label === 'DETAIL') {
+            // We're not doing anything with the details yet.
+        } else if (request.label === 'CATEGORY') {
+            // We are now on a category page. We can use this to paginate through and enqueue all products,
+            // as well as any subsequent pages we find
 
-        // Add links to the queue, but only from
-        // elements matching the provided selector.
-        await enqueueLinks({
-            selector: '.collection-block-item',
-            label: 'CATEGORY',
-        });
+            await page.waitForSelector('.product-item > a');
+            await enqueueLinks({
+                selector: '.product-item > a',
+                label: 'DETAIL', // <= note the different label
+            });
+
+            // Now we need to find the "Next" button and enqueue the next page of results (if it exists)
+            const nextButton = await page.$('a.pagination__next');
+            if (nextButton) {
+                await enqueueLinks({
+                    selector: 'a.pagination__next',
+                    label: 'CATEGORY', // <= note the same label
+                });
+            }
+        } else {
+            // This means we're on the start page, with no label.
+            // On this page, we just want to enqueue all the category pages.
+
+            await page.waitForSelector('.collection-block-item');
+            await enqueueLinks({
+                selector: '.collection-block-item',
+                label: 'CATEGORY',
+            });
+        }
     },
 });
 
