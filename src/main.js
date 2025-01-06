1ca67905746901410fc1be8413af934f3ed77651
Angie Byron <webchick@webchick.net>
2025-01-05 23:57:56 -0800

    Add enqueueLinks() to avoid searching manually for hrefs

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-// You don't need to import RequestQueue anymore
 import { CheerioCrawler } from 'crawlee';
 
 const crawler = new CheerioCrawler({
-    async requestHandler({ $, request }) {
+    // Let's limit our crawls to make our
+    // tests shorter and safer.
+    maxRequestsPerCrawl: 20,
+    // enqueueLinks is an argument of the requestHandler
+    async requestHandler({ $, request, enqueueLinks }) {
         const title = $('title').text();
         console.log(`The title of "${request.url}" is: ${title}.`);
+        // The enqueueLinks function is context aware,
+        // so it does not require any parameters.
+        await enqueueLinks();
     },
-    // Be kind.
-    maxRequestsPerCrawl: 20,
-})
+});
 
-// Start the crawler with the provided URLs
 await crawler.run(['https://crawlee.dev']);
