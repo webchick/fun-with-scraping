73b8103b61d37f4543b60d5f5c8e7f794b8fa46c
Angie Byron <webchick@webchick.net>
2025-01-06 01:02:50 -0800

    Grab specific product elements: price, sku, manufeacturer, etc.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,42 @@ const crawler = new PlaywrightCrawler({
     requestHandler: async ({ page, request, enqueueLinks }) => {
         console.log(`Processing: ${request.url}`);
         if (request.label === 'DETAIL') {
-            // We're not doing anything with the details yet.
+            const urlPart = request.url.split('/').slice(-1); // ['sennheiser-mke-440-professional-stereo-shotgun-microphone-mke-440']
+            const manufacturer = urlPart[0].split('-')[0]; // 'sennheiser'
+
+            const title = await page.locator('.product-meta h1').textContent();
+            const sku = await page.locator('span.product-meta__sku-number').textContent();
+
+            const priceElement = page
+                .locator('span.price')
+                .filter({
+                    hasText: '$',
+                })
+                .first();
+
+            const currentPriceString = await priceElement.textContent();
+            const rawPrice = currentPriceString.split('$')[1];
+            const price = Number(rawPrice.replaceAll(',', ''));
+
+            const inStockElement = page
+                .locator('span.product-form__inventory')
+                .filter({
+                    hasText: 'In stock',
+                })
+                .first();
+
+            const inStock = (await inStockElement.count()) > 0;
+
+            const results = {
+                url: request.url,
+                manufacturer,
+                title,
+                sku,
+                currentPrice: price,
+                availableInStock: inStock,
+            };
+
+            console.log(results);
         } else if (request.label === 'CATEGORY') {
             // We are now on a category page. We can use this to paginate through and enqueue all products,
             // as well as any subsequent pages we find
@@ -34,6 +69,9 @@ const crawler = new PlaywrightCrawler({
             });
         }
     },
+
+    // Let's limit our crawls to make our tests shorter and safer.
+    maxRequestsPerCrawl: 50,
 });
 
 await crawler.run(['https://warehouse-theme-metal.myshopify.com/collections']);
