de0248697550352c43ed1e75bc8952cdf4e7dd61
Angie Byron <webchick@webchick.net>
2025-01-05 23:49:39 -0800

    Simplify the example; RequestQueue is implicit.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
-// Add import of CheerioCrawler
-import { RequestQueue, CheerioCrawler } from 'crawlee';
+// You don't need to import RequestQueue anymore
+import { CheerioCrawler } from 'crawlee';
 
-const requestQueue = await RequestQueue.open();
-await requestQueue.addRequest({ url: 'https://crawlee.dev' });
-
-// Create the crawler and add the queue with our URL
-// and a request handler to process the page.
 const crawler = new CheerioCrawler({
-    requestQueue,
-    // The `$` argument is the Cheerio object
-    // which contains parsed HTML of the website.
     async requestHandler({ $, request }) {
-        // Extract <title> text with Cheerio.
-        // See Cheerio documentation for API docs.
         const title = $('title').text();
         console.log(`The title of "${request.url}" is: ${title}.`);
     }
 })
 
-// Start the crawler and wait for it to finish
-await crawler.run();
+// Start the crawler with the provided URLs
+await crawler.run(['https://crawlee.dev']);
